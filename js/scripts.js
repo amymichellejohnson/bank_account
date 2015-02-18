5d5928299fce9fe2730d94a726a7a1159c13d315
Amy Johnson and Joey Sandine <jasandine@gmail.com>
2015-02-18 12:10:17 -0800

    Add ability to add and subtract money from a bank balance and to display it up to 2 decimal places

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,44 @@ var BankAccount = {
   balance: 0,
   deposit: function(amount) {
     this.balance += amount;
+  },
+  withdraw: function(amount) {
+    this.balance -= amount;
   }
+
 };
+
+$(function() {
+  $("form#sign-up").submit(function(event) {
+    event.preventDefault();
+
+    var name = $("input#name").val();
+    var initialDeposit = $("input#initial-deposit").val().length ? parseFloat($("input#initial-deposit").val()) : 0;
+
+    var newBankAccount = Object.create(BankAccount);
+    newBankAccount.name = name;
+    newBankAccount.deposit(initialDeposit);
+
+    $("input#name").val("");
+    $("input#initial-deposit").val("");
+    $("#balance").text("$" + newBankAccount.balance.toFixed(2));
+
+    $("form#deposit-withdraw").submit(function(event) {
+      event.preventDefault();
+
+      var depositAmount = $("input#deposit-amt").val().length ? parseFloat($("input#deposit-amt").val()) : 0;
+      var withdrawAmount = $("input#withdraw-amt").val().length ? parseFloat($("input#withdraw-amt").val()) : 0;
+
+
+      newBankAccount.deposit(depositAmount);
+      newBankAccount.withdraw(withdrawAmount);
+
+
+      $("input#deposit-amt").val("");
+      $("input#withdraw-amt").val("");
+      $("#balance").text("$" + newBankAccount.balance.toFixed(2));
+    });
+
+  });
+
+});
